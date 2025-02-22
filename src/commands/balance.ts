f5a157bc13e3f34910c74126cbcc007602f3501e
Hydradevx <themaskedprogrammer1234@gmail.com>
2025-02-22 15:00:31 +0530

    Added Better Schema

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -1,21 +1,21 @@
-import { CommandInteraction, EmbedBuilder } from 'discord.js'
+import { CommandInteraction, EmbedBuilder } from "discord.js";
 
 export const data = {
-  name: 'balance',
-  description: 'See your balance',
-}
+  name: "balance",
+  description: "See your balance",
+};
 
 export async function execute(interaction: CommandInteraction, data) {
-  const { balance } = data
+  const { balance, bank } = data;
 
   const embed = new EmbedBuilder()
-    .setTitle(`<@&${interaction.user.id}>'s balance`)
-    .setDescription(`Balance: ${balance}`)
-    .setColor('#6f00e6')
+    .setTitle(`<@${interaction.user.id}>'s Balance`)
+    .setDescription(`💰 Wallet: ${balance}\n🏦 Bank: ${bank}`)
+    .setColor("#6f00e6");
 
   try {
-    await interaction.reply({ embeds: [embed] })
+    await interaction.reply({ embeds: [embed] });
   } catch (error) {
-    console.error('Error replying to interaction:', error)
+    console.error("Error replying to interaction:", error);
   }
 }
